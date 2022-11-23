7d1a6ac159da6ae067accd7c230ecc0d91e51fd8
leehyewon <tnddl2345@gmail.com>
2022-11-24 00:02:28 +0900

    [modify]

---
+++ js/article_create.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+    load_article();
+}
+
+async function load_article(){
+    const response = await fetch ('http://127.0.0.1:8000/articles/imgtoop/2/',{
+        headers : {
+            'Authorization' : 'Bearer ' + localStorage.getItem('access'),
+            'content-type' : 'application/json',
+        },
+        method : 'GET',
+    })
+
+    response_json = await response.json()
+    console.log(response_json)
+
+    const img = document.createElement('img')
+    img.src = `http://127.0.0.1:8000${response_json.output_image}` 
+    img.style.display = 'flex';
+    img.style.width = '300px';
+    img.style.margin = '20px';
+
+    const img_div = document.getElementById('output_img_tag')
+    img_div.appendChild(img)
+
+}
+
+async function create_article(){
+        
+    const title = document.getElementById('title').value
+    const content = document.getElementById('content').value
+    console.log(title, content, response_json.id)
+
+    const response = await fetch ('http://127.0.0.1:8000/articles/imgtoop/2/',{
+        headers : {
+            'Authorization' : 'Bearer ' + localStorage.getItem('access'),
+            'content-type' : 'application/json',
+        },
+        method : 'POST',
+        body : JSON.stringify({
+            "title":title,
+            "content":content,
+            "img":response_json.id
+        })
+    })
+}
